d906505c3ca82a35ba3238e1f93703d410898070
admin <admin@bitaps.com>
2018-01-10 21:18:48 +0400

    sec256k1, refactoring

--- test.py
+++ test.py
@@ -4,5 +4,5 @@ import test
 testLoad = unittest.TestLoader()
 suites = testLoad.loadTestsFromModule(test)
 
-runner = unittest.TextTestRunner(verbosity=2)
+runner = unittest.TextTestRunner(verbosity=3)
 runner.run(suites)
